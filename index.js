eb06b9c10d4b3bc7d26933487a5a1554340c5133
Betty Bialkowska <betty_1980_live@outlook.com>
2021-04-15 00:03:58 +0200

    oefening 1-js

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("Hello Winc Academy");
+let name = "Betty";
+console.log(name);
+var sum = 4 + 4;
+ console.log(sum);
+ var sum = "4" + "4"; //telt niet op
+console.log(sum);
+var sum = 1 + 4;
+ console.log(sum);
+ var sum = 2 * 4;
+ console.log(sum);
+ var sum = 10 - 4;
+ console.log(sum);
+ var sum = 10 % 4;
+ console.log(sum);
+var age = 48;
+console.log(age);
+var age = "48";
+console.log(age);
