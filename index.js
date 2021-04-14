cb9b28975aad3e3a4cfc3e7904f94e11e5982d50
Betty Bialkowska <betty_1980_live@outlook.com>
2021-04-15 00:10:15 +0200

    einde oefening

--- index.js
+++ index.js
@@ -16,4 +16,26 @@ var sum = 1 + 4;
 var age = 48;
 console.log(age);
 var age = "48";
-console.log(age);
+
+// oplossing
+// console.log(age);
+// console.log("Hello Winc Academy");
+// let name = "Marjolein";
+// console.log(name);
+// // Je kunt geen variabele aanmaken met de naam if.
+// let sum = 3 + 4;
+// console.log("sum:", sum);
+// let sumWithStrings = "3" + "4";
+// console.log("sum with strings:", sumWithStrings);
+// let subtraction = 10 - 3;
+// console.log("subtraction:", subtraction);
+// let multiplication = 1483 * 6342;
+// console.log("multiplication:", multiplication);
+// let division = 12 / 3;
+// console.log("division:", division);
+// let modulus = 12 % 9;
+// console.log("modulus:", modulus);
+// let age = 48;
+// console.log(typeof age);
+// let ageAsString = "48";
+// console.log(typeof ageAsString);
